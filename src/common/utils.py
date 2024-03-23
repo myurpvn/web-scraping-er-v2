1e50f9535d478409b00db1c039fc18f2243df52b
myuR <myurpvn@gmail.com>
2024-03-23 23:10:55 +0530

    refactor code

--- src/common/utils.py
+++ src/common/utils.py
@@ -6,8 +6,8 @@ from src.common.logger import logger
 tz = pytz.timezone("Asia/Colombo")
 
 
-def update_last_runtime() -> None:
+def update_last_run() -> None:
     time = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
     logger.info("Updating last run time")
-    with open("last_runtime.txt", "w") as f:
+    with open("last_run.txt", "w") as f:
         f.write(time)
