49e0129a35f7acd455ceedeabe43e4e0e4fb788f
myuR <myurpvn@gmail.com>
2024-04-04 20:17:09 +0530

    refactor code

--- src/web_scraping_er_v2/pipeline.py
+++ src/web_scraping_er_v2/pipeline.py
@@ -2,19 +2,27 @@ from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 
-from web_scraping_er_v2.maps import get_cc_map_country_list, generate_cc_map_df, read_sub_map
+from web_scraping_er_v2.maps import (
+    get_cc_map_country_list,
+    generate_cc_map_df,
+    read_sub_map,
+)
 from src.web_scraping_er_v2.load_to_bq import bq_load_daily, bq_load_map
-from src.discord_integration.send_message import send_simple_text, send_dataframe_as_text
+from src.discord_integration.send_message import (
+    send_simple_text,
+    send_dataframe_as_text,
+)
 from src.common.utils import update_last_run
 from src.common.logger import logger
 
+
 def pipeline(base_currency):
     logger.info("Staring script", base_currency=base_currency)
-    
+
     logger.info("Getting website data")
     exchange_rate_map = {}
     response = get(f"https://www.x-rates.com/table/?from={base_currency}&amount=1")
-    
+
     logger.info("Extracting data from website")
     is_updated = False
     soup = BeautifulSoup(response.content, "html.parser")
@@ -39,7 +47,7 @@ def pipeline(base_currency):
 
     logger.info("Building DataFrame")
     exchange_rates_df = pd.DataFrame(
-        data=exchange_rate_map.values(), index=exchange_rate_map.keys()
+        data=exchange_rate_map.values(), index=pd.Index(exchange_rate_map.keys())
     )
 
     # exchange_rates_df.to_csv("sample_rates.csv", index=False)
@@ -47,12 +55,15 @@ def pipeline(base_currency):
     result = bq_load_daily(exchange_rates_df, base_currency)
     if result["status"] == "FAILED":
         logger.info("BQ Load Failed", exception=result["error"])
-        send_simple_text(f"BQ Daily run Failed,  error: {result["error"]}")
+        error = result["error"]
+        send_simple_text(f"BQ Daily run Failed,  error: {error}")
     else:
         # checking and sending subscribed exchange-rates
         sub_list = read_sub_map()
         logger.info("Sending ex-rates", subscribed=sub_list)
-        discord_df = exchange_rates_df[exchange_rates_df["foreign_currency"].isin(sub_list)]
+        discord_df = exchange_rates_df[
+            exchange_rates_df["foreign_currency"].isin(sub_list)
+        ]
         send_dataframe_as_text(discord_df)
 
     if is_updated:
@@ -63,4 +74,3 @@ def pipeline(base_currency):
         # send_simple_text(f"BQ Map Data Loading: {result["status"]}, error: {result["error"]}")
 
     update_last_run()
-    
